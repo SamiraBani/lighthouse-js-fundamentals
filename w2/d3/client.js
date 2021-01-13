3d1febe2dda9eb1028fb49a646bbf42ba06a71a7
SamiraBani <banihassan.s@gmail.com>
2021-01-13 01:06:41 +0000

    set the game up

---
+++ w2/d3/client.js
@@ -0,0 +1,24 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '10.0.2.15',
+    port: 50541
+  });
+  conn.on('connect', () => {console.log('connection established successfully')});
+  conn.name = "Name : SNK";
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  conn.on('data', message => {console.log("you ded cuz you idled!")});
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
+
+
+
+module.exports = {connect};
